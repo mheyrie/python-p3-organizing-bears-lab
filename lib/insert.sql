bf328099736f912bce39c24c6f2a81a08fe98030
Mary <omota4fidelity@gmail.com>
2023-10-18 01:21:30 +0100

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) 
+VALUES 
+("Mr. Chocolate", 5, "M", "White Asis", "Introvert", 0), 
+("Rowdy", 8, "M", "Black", "Extrovert", 1),
+("Tabitha", 6, "F", "Black", "Extrovert", 1),
+("Sergeant Brown", 5, "F", "Black", "Extrovert", 1),
+("Melissa", 12, "M", "Black", "Introvert", 0),
+("Grinch", 4, "F", "Black", "Extrovert", 1),
+("Wendy", 2, "F", "Black American", "Extrovert", 1),
+(NULL, NULL, NULL, NULL, NULL, NULL); 
